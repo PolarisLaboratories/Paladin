51e6569874b7c1b46863c2560c632f5db0b2e25f
Jason Lu <jasonbangbang@gmail.com>
2017-04-27 16:44:19 -0500

    Rename ws.js to wss.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 
-var ws = require('./lib/ws.js');
+var ws = require('./lib/wss.js');
 
 var app = express();
 
