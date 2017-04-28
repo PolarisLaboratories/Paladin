d395fe3db6449d8904d2d287f4d093a5c18b7a52
Jason Lu <jasonbangbang@gmail.com>
2017-04-28 14:10:52 -0500

    Close socket on tab close

--- public/javascripts/ws.js
+++ public/javascripts/ws.js
@@ -1,12 +1,16 @@
 class PaladinWebSocket {
     constructor(address) {
         this.ws = new WebSocket(address);
-        this.ws.onmessage = this.ws_handler;
+        this.ws.onmessage = this.handler;
+        window.close = this.close;
     }
-    ws_handler(event) {
+    handler(event) {
         console.log(event.data);
     }
     write(data) {
         this.ws.send(data);
+    }
+    close() {
+        this.ws.close();
     }
 }
