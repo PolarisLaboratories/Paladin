0221db43c746ce2c06ffcbd27b15456e10010440
Jason Lu <jasonbangbang@gmail.com>
2017-05-09 12:34:12 -0500

    Respect banner timeouts

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -14,7 +14,7 @@ var g;
 function banner(type, message, delay) {
     $('body').prepend('<div style="padding: 5px; z-index: 10; position: absolute; right: 0; left: 0;"> <div id="inner-message" class="alert ' + type + ' show"><button type="button" class="close" data-dismiss="alert" aria-label="Close"> <span aria-hidden="true">&times;</span></button>' + message + '</div></div>');
     if (delay >= 0) {
-        $(".alert").delay(3000).fadeOut("slow", function () { $(this).parent('div').remove(); $(this).remove(); });
+        $(".alert").delay(delay).fadeOut("slow", function () { $(this).parent('div').remove(); $(this).remove(); });
     }
 }
 
